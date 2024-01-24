ee12087ab30ca33d910c2dcb7854d0304051df2a
Allen Wang <1097060357@qq.com>
2024-01-25 02:25:34 +0800

    24-01-25

---
+++ 剑指II/24.swift
@@ -0,0 +1,24 @@
+// 反转链表
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var pre: ListNode? = nil
+        var cur = head
+        while cur != nil {
+            var next = cur?.next
+            cur?.next = pre
+            pre = cur
+            cur = next
+        }
+        return pre
+    }
+}
