e5b11ca0071d8084b05671d030ee04b8cd74d839
Allen Wang <1097060357@qq.com>
2024-02-16 05:36:28 +0800

    24-02-16

---
+++ Leetcode/Leetcode_1920.cpp
@@ -0,0 +1,14 @@
+// 基于排列构建数组
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            nums[i] += 1000 * (nums[nums[i]] % 1000);
+        }
+        for (int i = 0; i < n; ++i) {
+            nums[i] /= 1000;
+        }
+        return nums;
+    }
+};
